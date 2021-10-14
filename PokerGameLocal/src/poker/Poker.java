5cde138925b6e035a9b3734565e0da7d38987f8e
lkyaf <ckylau0208@gmail.com>
2021-10-15 00:58:38 +0800

    Version 0 - PokerGame from CS3343 Website

---
+++ PokerGameLocal/src/poker/Poker.java
@@ -0,0 +1,42 @@
+package poker;
+
+public class Poker {
+	// Precondition: A hand of n cards have been sorted by card number.
+	public boolean isFullHouse(String cards[], int n) {
+		return isThreeOfaKind(cards, n) && isTwoPairs(cards, n);
+	}
+
+	// Precondition: A hand of n cards have been sorted by card number.
+	public boolean isThreeOfaKind(String cards[], int n) {
+		for (int i=0; i<n-2; i++) {
+			if (cards[i].charAt(1) == cards[i+1].charAt(1) &&
+					cards[i+1].charAt(1) == cards[i+2].charAt(1))
+					if (i<2 && cards[i+2].charAt(1)== cards[i+3].charAt(1)) {return false;}
+					else if (i>2 && cards[i+2].charAt(1)== cards[0].charAt(1)) {return false;}
+					return true;
+		}
+
+		return false;
+	}
+
+	// Precondition: A hand of n cards have been sorted by card number.
+	public boolean isTwoPairs(String cards[], int n) {
+		int count = 0;
+		for (int i=0; i<n-1; i++) {
+			if (cards[i].charAt(1) == cards[i+1].charAt(1)) {
+				count++;
+				i++;
+			}
+		}
+		if (count == 2)
+			return true;
+		else
+			return false;
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(new Poker().isFullHouse(
+			new String[] {"CJ", "DJ", "HK", "SK", "DK"}, 5));
+	}
+}
+// end of Poker.java
